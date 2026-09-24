2a81222795d70b04c072f5c0b92b991b5ae6836f
P <Programgramprobio@gmail.com>
2026-09-25 04:12:47 +0700

    cell06

---
+++ discovery_piscine/cell06/ex02/downcase_all.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def downcase_it(s):
+    return s.lower()
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("none")
+    else:
+        for arg in sys.argv[1:]:
+            print(downcase_it(arg))
+
+
+if __name__ == "__main__":
+    main()
